c5ea8a114a810ce5d2e6fcbb9719d3851e8cd62f
xaknem <younube93@gmail.com>
2017-03-08 22:53:14 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,20 +1,26 @@
 from tkinter import *
-#импортируем необходимые модули из библиотеки
-def button_clicked(x):
-	print(len(x))
+from datetime import datetime, date, time
+import os
 
+def notyet(event):
+    _field.delete(1.0, END)
+    a = len(insert_field.get())
+    _field.insert(1.0, a)
 root = Tk()
+root.title('B-Double Client')
+root.geometry('800x400+300+200')
+root.resizable(True, True)
 
-ent = Entry()
-ent.pack()
+menu_frame = Frame(root ,bg='green',bd=5).pack(side=TOP, expand = YES, fill = BOTH)
+active_frame1 = Frame(root,height=25, background = 'red', borderwidth = 15).pack(side=BOTTOM)
+#insert
 
-button1 = Button()
-button1.pack()
+_field = Text(active_frame1, height=10, width=7, font='Arial 11', wrap=WORD)
+_field.pack(expand=NO, fill = 'x')
 
-button2 = Button(root, bg='red', text = u"CLICK", command = button_clicked(ent))
-button2.pack()
-
-okno = Label()
-okno.pack()
-
+button = Button(active_frame1, text='ok',bg='black',fg='red',font='arial 7', command = notyet)
+button.pack(side = RIGHT)
+insert_field = Entry(active_frame1, font='Arial 11')
+insert_field.pack(fill = 'x')
+insert_field.bind('<Return>', notyet)
 root.mainloop()
